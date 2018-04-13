554691e038ee5eca03d030b65e8a97f16bcd55bd
Helvio Pedreschi <helvio88@gmail.com>
2018-04-13 17:01:08 -0400

    Update kevo-unofficial.groovy

--- devicetypes/kennelbound-smartthings/kevolock/kevo-unofficial.src/kevo-unofficial.groovy
+++ devicetypes/kennelbound-smartthings/kevolock/kevo-unofficial.src/kevo-unofficial.groovy
@@ -7,7 +7,8 @@ metadata {
         capability "Health Check"
         capability "Lock"
         capability "Refresh"
-
+        capability "Switch"
+        
         attribute 'firmware', 'string'
     }
 
